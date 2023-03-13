71ae07b39635a0f6247e9e7f6c7a6918cf292c80
Kayke Vieira <kaykev559@gmail.com>
2023-03-13 17:43:56 -0300

    esaqueci o add

---
+++ Projeto_Facens/script.js
@@ -0,0 +1,14 @@
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+/*function mostrar(){
+    let img = document.getElementById(foto)
+    src = "imagem1.jpeg"
+    onmouseover('imagem1.jpeg')
+}*/
+
+//classe é a forma de bolo
+//Instância é o objeto, o que sai dentro do bolo é o objeto, característicasque diferem os objetos um do outro
